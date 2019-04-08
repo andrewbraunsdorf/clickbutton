c09d2de9fbd30f150c401673843d4e3fafdcde60
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-08 22:22:19 +0000

    added mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
-// start the express web server listening on 8080
-app.listen(8080, () => {
-  console.log('listening on 8080');
+// connect to the db and start the express server
+let db;
+
+const url =  'mongodb://localhost:8080/lotsofclicks';
+
+MongoClient.connect(url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  // start the express web server listening on 8080
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  });
 });
 
 // serve the homepage
